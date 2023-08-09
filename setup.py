446e151a71b4dbf5479f170986b74b7d9722dd99
danielhe-chartsfigures <141799661+danielhe-chartsfigures@users.noreply.github.com>
2023-08-09 01:28:42 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'chartsfigures',
   packages = ['chartsfigures'],
-  version = '0.01',
+  version = '0.02',
   license='MIT',
   description = 'Social Science data analysis',
   author = 'Daniel He',
   author_email = 'danielhe2024@u.northwestern.edu',
-  url = 'https://github.com/joelbarmettlerUZH/Scrapeasy',
-  download_url = 'https://github.com/joelbarmettlerUZH/Scrapeasy/archive/pypi-0_1_3.tar.gz',
+  url = 'https://github.com/danielhe-chartsfigures/chartsfigures',
+  download_url = 'https://github.com/danielhe-chartsfigures/chartsfigures',
   keywords = ['charts', 'figures', 'social science'],
   install_requires=[
           'validators',
